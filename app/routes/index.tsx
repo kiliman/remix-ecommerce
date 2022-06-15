8227331de3dc3f3c5baeabc24597c8ac25251c6d
kiliman <kiliman@gmail.com>
2022-06-15 16:13:27 -0400

    🔥 Remove notes routes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -30,10 +30,10 @@ export default function Index() {
               <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
                 {user ? (
                   <Link
-                    to="/notes"
+                    to="/products"
                     className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
                   >
-                    View Notes for {user.email}
+                    View Products for {user.email}
                   </Link>
                 ) : (
                   <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
