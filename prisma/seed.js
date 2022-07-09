f195001a3a628ee57c4192731489e91aa66aca91
kiliman <kiliman@gmail.com>
2022-07-09 11:26:53 -0400

    🔨 Update seed file since cart no longer requires user

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,30 +1,10 @@
 const { PrismaClient } = require("@prisma/client");
 const products = require("./products.json");
-const bcrypt = require("bcryptjs");
 
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = "rachel@remix.run";
-
-  const userCount = await prisma.user.count();
   const productCount = await prisma.product.count();
-
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
-
-  if (userCount === 0) {
-    // create default user
-    await prisma.user.create({
-      data: {
-        email,
-        password: {
-          create: {
-            hash: hashedPassword,
-          },
-        },
-      },
-    });
-  }
   if (productCount === 0) {
     for (let product of products) {
       await prisma.product.create({
